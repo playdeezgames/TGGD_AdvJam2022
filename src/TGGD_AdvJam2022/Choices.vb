8bdd536137ae39a33fb3e1d48f482abb82fa4f33
TheGrumpyGameDev <ernestpazera@msn.com>
2022-06-04 04:40:37 -0500

    confirm quittinhg

--- src/TGGD_AdvJam2022/Choices.vb
+++ src/TGGD_AdvJam2022/Choices.vb
@@ -1,3 +1,6 @@
 ﻿Friend Module Choices
+    Friend Const NewGameText = "New Game"
+    Friend Const NoText = "No"
     Friend Const QuitText = "Quit"
+    Friend Const YesText = "Yes"
 End Module
--- src/TGGD_AdvJam2022/Program.vb
+++ src/TGGD_AdvJam2022/Program.vb
@@ -1,8 +1,7 @@
-Imports System
-
 Module Program
-    Sub Main(args As String())
+    Sub Splash()
         Console.Title = "A Game in VB.NET About Adventure!"
+        AnsiConsole.Clear()
         Dim figlet As New FigletText("A Game in VB.NET About Adventure!") With
             {
                 .Alignment = Justify.Center,
@@ -10,15 +9,27 @@ Module Program
             }
         AnsiConsole.Write(figlet)
         AnsiConsole.WriteLine("A production of TheGrumpyGameDev")
-
+    End Sub
+    Function ConfirmQuit() As Boolean
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = "[red]Are you sure you want to quit?[/]"}
+        prompt.AddChoice(NoText)
+        prompt.AddChoice(YesText)
+        Return AnsiConsole.Prompt(prompt) = YesText
+    End Function
+    Sub MainMenu()
         Dim done = False
         While Not done
-            Dim mainMenu As New SelectionPrompt(Of String) With {.Title = ""}
+            Dim mainMenu As New SelectionPrompt(Of String) With {.Title = "Main Menu"}
+            mainMenu.AddChoice(NewGameText)
             mainMenu.AddChoice(QuitText)
             Select Case AnsiConsole.Prompt(mainMenu)
                 Case QuitText
-                    done = True
+                    done = ConfirmQuit()
             End Select
         End While
+    End Sub
+    Sub Main(args As String())
+        Splash()
+        MainMenu()
     End Sub
 End Module
